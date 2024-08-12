5d41d5ba3d0229de33be47f194ee1d19cc7954d6
Andranik Virabyan <gmparstone99@gmail.com>
2024-08-12 11:16:54 -0800

    Initial and Final working code

---
+++ src/api/fake_data.js
@@ -0,0 +1,247 @@
+export const FAKE_POPULARS = [
+  {
+    adult: false,
+    backdrop_path: '/uCY1j1YqfDWRbbS7hJwd9szX1sJ.jpg',
+    first_air_date: '2024-04-15',
+    genre_ids: (3)[(10766, 18, 35)],
+    id: 237480,
+    name: 'No Rancho Fundo',
+    origin_country: ['BR'],
+    original_language: 'pt',
+    original_name: 'No Rancho Fundo',
+    overview: 'Across more than a decade, 17 teen boys and young men were murdered by convicted killer Jeffrey Dahmer. How did he evade arrest for so long?',
+    popularity: 2737.146,
+    poster_path: '/eONkvEahSQJan1HTzWJKjvaMe29.jpg',
+    vote_average: 5.094,
+    vote_count: 16,
+  },
+]
+
+export const FAKE_RECOMMENDATIONS = [
+  {
+      "backdrop_path": "/va6iR1UBVvbj9xFKbK1XHvALb98.jpg",
+      "id": 233952,
+      "name": "Golden Trio",
+      "original_name": "老家伙",
+      "overview": "Xiao Chang Qing, Sun Qian Cheng and Chen Xin Cheng worked hard all their lives. After retirement, they finally got together and co-founded a warm retirement community. In the community, they solved problems that elderly people faced, and in the process, they recognized their own problems and reconciled with their family and with themselves",
+      "poster_path": "/4edgc2TuV3PP7FvFV2zzWDRagNP.jpg",
+      "media_type": "tv",
+      "adult": false,
+      "original_language": "zh",
+      "genre_ids": [
+          18
+      ],
+      "popularity": 53.986,
+      "first_air_date": "2024-05-14",
+      "vote_average": 6.1,
+      "vote_count": 9,
+      "origin_country": [
+          "CN"
+      ]
+  },
+  {
+      "backdrop_path": "/bIhmqQNXcyWRzH153d3jaCbLTy3.jpg",
+      "id": 244643,
+      "name": "El amor no tiene receta",
+      "original_name": "El amor no tiene receta",
+      "overview": "Paz Roble, a kind, hard-working and honest woman from whom her husband, Fermín, takes her newborn daughter. Fermín has a large debt with some lenders and to pay it he decides to sell his daughter to Mauro Nicoliti. This plagiarism is orchestrated by Geneva, Mauro's adoptive sister. She lost her first-born daughter in childbirth and does everything to replace her dead daughter and keep her husband's fortune. On the other hand, Esteban Villa de Cortés loses his wife and becomes a widower with his three children. Elvira Moncada, Esteban's mother-in-law, holds him responsible for the death of her daughter. After a series of unexpected events, Esteban and Paz meet in the neighborhood where she lives with her family. The chemistry and great understanding that exists between them makes it inevitable that they will fall in love.",
+      "poster_path": "/fDRy8B1KdapuvBsgkCkEETY4MNr.jpg",
+      "media_type": "tv",
+      "adult": false,
+      "original_language": "es",
+      "genre_ids": [
+          10759,
+          10765,
+          18,
+          10751,
+          10766,
+          35
+      ],
+      "popularity": 116.95,
+      "first_air_date": "2024-02-19",
+      "vote_average": 7.6,
+      "vote_count": 55,
+      "origin_country": [
+          "MX"
+      ]
+  },
+  {
+      "backdrop_path": "/zZqpAXxVSBtxV9qPBcscfXBcL2w.jpg",
+      "id": 1399,
+      "name": "Game of Thrones",
+      "original_name": "Game of Thrones",
+      "overview": "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+      "poster_path": "/1XS1oqL89opfnbLl8WnZY1O1uJx.jpg",
+      "media_type": "tv",
+      "adult": false,
+      "original_language": "en",
+      "genre_ids": [
+          10765,
+          18,
+          10759
+      ],
+      "popularity": 817.772,
+      "first_air_date": "2011-04-17",
+      "vote_average": 8.454,
+      "vote_count": 23625,
+      "origin_country": [
+          "US"
+      ]
+  },
+  {
+      "backdrop_path": "/x4salpjB11umlUOltfNvSSrjSXm.jpg",
+      "id": 1402,
+      "name": "The Walking Dead",
+      "original_name": "The Walking Dead",
+      "overview": "Sheriff's deputy Rick Grimes awakens from a coma to find a post-apocalyptic world dominated by flesh-eating zombies. He sets out to find his family and encounters many other survivors along the way.",
+      "poster_path": "/n7PVu0hSz2sAsVekpOIoCnkWlbn.jpg",
+      "media_type": "tv",
+      "adult": false,
+      "original_language": "en",
+      "genre_ids": [
+          10759,
+          18,
+          10765
+      ],
+      "popularity": 411.056,
+      "first_air_date": "2010-10-31",
+      "vote_average": 8.115,
+      "vote_count": 16280,
+      "origin_country": [
+          "US"
+      ]
+  },
+  {
+      "backdrop_path": "/scx72FpTF929jYXea0MphyE24Wa.jpg",
+      "id": 46648,
+      "name": "True Detective",
+      "original_name": "True Detective",
+      "overview": "An American anthology police detective series utilizing multiple timelines in which investigations seem to unearth personal and professional secrets of those involved, both within or outside the law.",
+      "poster_path": "/cuV2O5ZyDLHSOWzg3nLVljp1ubw.jpg",
+      "media_type": "tv",
+      "adult": false,
+      "original_language": "en",
+      "genre_ids": [
+          18
+      ],
+      "popularity": 244.509,
+      "first_air_date": "2014-01-12",
+      "vote_average": 8.3,
+      "vote_count": 3308,
+      "origin_country": [
+          "US"
+      ]
+  },
+  {
+      "backdrop_path": "/7cqKGQMnNabzOpi7qaIgZvQ7NGV.jpg",
+      "id": 76479,
+      "name": "The Boys",
+      "original_name": "The Boys",
+      "overview": "A group of vigilantes known informally as “The Boys” set out to take down corrupt superheroes with no more than blue-collar grit and a willingness to fight dirty.",
+      "poster_path": "/2zmTngn1tYC1AvfnrFLhxeD82hz.jpg",
+      "media_type": "tv",
+      "adult": false,
+      "original_language": "en",
+      "genre_ids": [
+          10765,
+          10759
+      ],
+      "popularity": 1008.354,
+      "first_air_date": "2019-07-25",
+      "vote_average": 8.476,
+      "vote_count": 10213,
+      "origin_country": [
+          "US"
+      ]
+  },
+  {
+      "backdrop_path": "/9faGSFi5jam6pDWGNd0p8JcJgXQ.jpg",
+      "id": 1396,
+      "name": "Breaking Bad",
+      "original_name": "Breaking Bad",
+      "overview": "Walter White, a New Mexico chemistry teacher, is diagnosed with Stage III cancer and given a prognosis of only two years left to live. He becomes filled with a sense of fearlessness and an unrelenting desire to secure his family's financial future at any cost as he enters the dangerous world of drugs and crime.",
+      "poster_path": "/ztkUQFLlC19CCMYHW9o1zWhJRNq.jpg",
+      "media_type": "tv",
+      "adult": false,
+      "original_language": "en",
+      "genre_ids": [
+          18,
+          80
+      ],
+      "popularity": 516.87,
+      "first_air_date": "2008-01-20",
+      "vote_average": 8.913,
+      "vote_count": 13972,
+      "origin_country": [
+          "US"
+      ]
+  },
+  {
+      "backdrop_path": "/iHSwvRVsRyxpX7FE7GbviaDvgGZ.jpg",
+      "id": 119051,
+      "name": "Wednesday",
+      "original_name": "Wednesday",
+      "overview": "Wednesday Addams is sent to Nevermore Academy, a bizarre boarding school where she attempts to master her psychic powers, stop a monstrous killing spree of the town citizens, and solve the supernatural mystery that affected her family 25 years ago — all while navigating her new relationships.",
+      "poster_path": "/9PFonBhy4cQy7Jz20NpMygczOkv.jpg",
+      "media_type": "tv",
+      "adult": false,
+      "original_language": "en",
+      "genre_ids": [
+          10765,
+          9648,
+          35
+      ],
+      "popularity": 165.217,
+      "first_air_date": "2022-11-23",
+      "vote_average": 8.472,
+      "vote_count": 8403,
+      "origin_country": [
+          "US"
+      ]
+  },
+  {
+      "backdrop_path": "/jbTqU6BJMufoMnPSlO4ThrcXs3Y.jpg",
+      "id": 1434,
+      "name": "Family Guy",
+      "original_name": "Family Guy",
+      "overview": "Sick, twisted, politically incorrect and Freakin' Sweet animated series featuring the adventures of the dysfunctional Griffin family. Bumbling Peter and long-suffering Lois have three kids. Stewie (a brilliant but sadistic baby bent on killing his mother and taking over the world), Meg (the oldest, and is the most unpopular girl in town) and Chris (the middle kid, he's not very bright but has a passion for movies). The final member of the family is Brian - a talking dog and much more than a pet, he keeps Stewie in check whilst sipping Martinis and sorting through his own life issues.",
+      "poster_path": "/y0HUz4eUNUe3TeEd8fQWYazPaC7.jpg",
+      "media_type": "tv",
+      "adult": false,
+      "original_language": "en",
+      "genre_ids": [
+          16,
+          35
+      ],
+      "popularity": 641.14,
+      "first_air_date": "1999-01-31",
+      "vote_average": 7.35,
+      "vote_count": 4270,
+      "origin_country": [
+          "US"
+      ]
+  },
+  {
+      "backdrop_path": "/xuCHkaxkkm9pPum1SByC0dhvR2U.jpg",
+      "id": 91363,
+      "name": "What If...?",
+      "original_name": "What If...?",
+      "overview": "Taking inspiration from the comic books of the same name, each episode of this animated anthology series questions, revisits and twists classic Marvel Cinematic moments.",
+      "poster_path": "/rWg4Jk7NwVEz2BtU1aKKDoDJPeB.jpg",
+      "media_type": "tv",
+      "adult": false,
+      "original_language": "en",
+      "genre_ids": [
+          16,
+          10759,
+          10765
+      ],
+      "popularity": 159.412,
+      "first_air_date": "2021-08-11",
+      "vote_average": 8.155,
+      "vote_count": 4101,
+      "origin_country": [
+          "US"
+      ]
+  }
+]
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const BASE_URL = 'https://api.themoviedb.org/3/'
+const API_KEY_PARAM = '?api_key=03be1b01d5bbeace9b20a3a8cb27236c'
+const BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/original'
+const SMALL_IMG_COVER_BASE_URL = 'https://image.tmdb.org/t/p/w300'
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL }
